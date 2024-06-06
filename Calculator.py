ba7da7ea4868ae1b5cf2dfbbcfdb5be05bf33953
yogurtslaps <myau32@uwo.ca>
2024-06-06 01:50:20 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+#In this assessment, you will create a calculator. Write a program that gets two inputs from the user and 
+#performs basic calculator functionalities on them (add, subtract, multiply and divide). 
+
+x=int(input("Enter first number:"))
+print("first_number:",x) 
+
+y=int(input("Enter second number:"))
+print("second_number:",y)
+
+
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+
+print(a)
+print(b)
+print(c)
+print(d)
