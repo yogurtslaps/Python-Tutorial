091b2f042442d2b22c37ad744489b2165c95eba1
yogurtslaps <myau32@uwo.ca>
2024-06-06 02:08:28 -0400

    Add files via upload

---
+++ Circumference.py
@@ -0,0 +1,11 @@
+#Write a program to calculate the area and circumference of a circle given the inputs from the user. 
+#You are given the required radius input from the user. Print out the area and circumference of the circle. 
+
+x=float(input("Enter radius:"))
+print("Radius:",x) 
+
+y=x*2*3.14
+print("Circumference:",y)
+
+z=x*3.14*(x**2)
+print("Area:",z) 
