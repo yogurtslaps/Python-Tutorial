78272fb1c57b272cb647e825b4a491b1fdd3e963
yogurtslaps <myau32@uwo.ca>
2024-06-05 23:36:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#Write a program to calculate age of a person/item. The program takes two inputs from the user
+#(year person born/item created and current year) and prints out the age of the person/item. 
+x=1998 #start_year
+y= 2024 #end-year
+a=y-x
+print(a)
