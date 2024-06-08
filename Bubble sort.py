4ff1c6e582aa8269e6db730307aed065e06f9a44
yogurtslaps <myau32@uwo.ca>
2024-06-08 18:13:38 -0400

    Add files via upload

---
+++ Bubble sort.py
@@ -0,0 +1,8 @@
+#Bubble sort is a comparison-based algorithm in which each pair of adjacent elements is compared and swapped, if they are not in order.
+#Write a program that sorts an array ascendingly using bubble sort. 
+
+ls=[2,7,19,0]
+
+ls.sort()
+print("Sorted accendingly:",ls)
+
